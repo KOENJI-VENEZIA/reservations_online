28a8a20b916d772c3853fb2470397001cf19480c
Matteo Nassini <mnas@bendingspoons.com>
2025-03-12 22:45:16 +0100

    [fix] directory references

--- public/js/utils/locale.js
+++ public/js/utils/locale.js
@@ -28,7 +28,7 @@ async function setLanguage(lang) {
     if (!translations[lang]) {
         try {
             // Load language file
-            const response = await fetch(`/public/locales/${lang}.json`);
+            const response = await fetch(`reservations_online/public/locales/${lang}.json`);
             
             if (!response.ok) {
                 throw new Error(`Failed to load language: ${lang}`);
